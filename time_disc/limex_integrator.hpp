8bab12b5bf2b038e10891fc2704a404a1881a2bc
DanGon <daniel.gonzalezesparza@kaust.edu.sa>
2026-06-23 01:25:43 +0300

    No big changes

--- time_disc/limex_integrator.hpp
+++ time_disc/limex_integrator.hpp
@@ -1191,22 +1191,22 @@ apply(SmartPtr<grid_function_type> u, number t1, ConstSmartPtr<grid_function_typ
 
 			m_vThreadData[0].get_solver()->step_update();
 			
-			UG_LOG("/n"<< std::endl);
+			UG_LOG("\n"<< std::endl);
 			UG_LOG("---------------------------------------------------"<< std::endl);
 			UG_LOG("+++ LimexTimestep +++" << m_limex_step-1 << " ENDED"<< std::endl);
 			UG_LOG("---------------------------------------------------"<< std::endl);
-			UG_LOG("/n/n"<< std::endl);
+			UG_LOG("\n\n"<< std::endl);
 			
 			
 		}
 		else
 		{
 			// DISCARD time step
-			UG_LOG("/n"<< std::endl);
+			UG_LOG("\n"<< std::endl);
 			UG_LOG("+++ LimexTimestep +++" << m_limex_step << " FAILED" << std::endl);
 			UG_LOG("               :\t time \t dt (failed) \t dt (curr) \teps=\t" << epsmin <<"\t(tol="<<m_tol<<")\terr="<<err<< std::endl);
 			UG_LOG("LIMEX-REJECTING:\t" << t <<"\t"<< dt << "\t" << dtcurr <<std::endl);
-			UG_LOG("/n/n"<< std::endl);
+			UG_LOG("\n\n"<< std::endl);
 
 			itime_integrator_type::notify_rewind_step(ubest, m_limex_step, t+dt, dt);
 
